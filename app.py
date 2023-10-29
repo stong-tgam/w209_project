2ebb665ce3420b4edc11b2a765ddc739193a5b26
Steven Tong <stong@Stevens-MacBook-Pro.local>
2023-10-28 22:27:03 -0400

    update htmls

--- app.py
+++ app.py
@@ -1,10 +1,20 @@
 from flask import Flask, render_template
-
 app = Flask(__name__)
 
 @app.route("/")
-def hello():
-    return render_template('index.html')
-
-if __name__ == "__main__":
-    app.run(debug = True, host = "0.0.0.0", port = 3000)
+def w209():
+    return render_template("homepage.html")
+@app.route("/content1")
+def content1():
+    return render_template("content1.html")
+@app.route("/content2")
+def content2():
+    return render_template("content2.html")
+@app.route("/content3")
+def content3():
+    return render_template("content3.html")
+@app.route("/dataset")
+def dataset():
+    return render_template("dataset.html")
+if __name__== '__main__':
+    app.run()
