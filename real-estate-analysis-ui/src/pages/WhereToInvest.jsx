c8a4f507512c134fa87b90dd280d2ecf0bf4ffa0
Steven Tong <stong@ip-192-168-0-24.ec2.internal>
2023-12-08 20:29:44 -0500

    update text on 1st page

--- real-estate-analysis-ui/src/pages/WhereToInvest.jsx
+++ real-estate-analysis-ui/src/pages/WhereToInvest.jsx
@@ -8,13 +8,30 @@ export default function Resources() {
         <div className="section">
           <div class="text">
             <h2>Price per Square Meter and Rent Change Over Years</h2>
-            <p>1. Asia Cities price per sqm increased rapidly over past years</p>
-            <p>2. Over the past years, the cost of renting a one-bedroom apartment in North American cities has increased, while the price per square meter has changed less relatively</p>
+            <p>Affordable Market: Low-cost living and buying, ideal for budget-focused buyers or investors.</p>
+            <p>Investor's Market: High property prices with lower rents, indicating potential for future value growth.</p>
+            <p>Renter's Market: High rents with more affordable purchase prices, attractive for rental income investors.</p>
+            <p>Premium Market: High rents and purchase prices, indicating a luxury, high-demand urban real estate sector.</p>
+            <p>Insight:</p>
+            <p>Hong Kong stands out with top-tier rents and property prices, hinting at a volatile market.
+                New York and Los Angeles show high rents but more moderate buying costs.
+                In contrast, Shanghai, Paris, Berlin, and Toronto present more balanced and lower real estate costs.
+            </p>
           </div>
         </div>
         <WhereToInvestTableau />
+        <div className="section">
+          <div class="text">
+            <h2>Section #2</h2>
+            <p>Global Rent Yields: There's a clear visual differentiation in rent yields between cities, Nigeria Lagos is the highest.</p>
+            <p>Trend Over Time: The rent yield graphs show a general decline in both countryside and downtown rent yields over the years, while countryside rent yields fluctuate but remain higher.</p>
+            <p> Commuting Trends: Commute times peaked around 2015 and have since improved, suggesting better transportation or changes in work habits.</p>
+          </div>
+        </div>
         <WhereToInvestTableauB />
         <div className='bottom-space'></div>
       </div>
     );
   }
+
+
